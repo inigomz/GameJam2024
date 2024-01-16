cc3eea1c9eb690a1bb0f6ccfa419bfc3ab382f3e
inigomz <inigomzulueta@gmail.com>
2024-01-16 15:28:08 -0800

    init
    initializing core files for the game

---
+++ Menu.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_new_game_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_options_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit() # Replace with function body.
